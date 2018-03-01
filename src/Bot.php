7095c93e0e137bd515bc08a84d6698fcc5d0a0c5
Vladyslav Zinko <vladyslav_zinko@epam.com>
2018-03-01 18:24:04 +0200

    added like for user

--- src/Bot.php
+++ src/Bot.php
@@ -5,10 +5,12 @@ namespace Bot;
 require __DIR__ . '/../vendor/autoload.php';
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Response;
 
 class Bot
 {
+    private static $client;
+    private $user;
+
     //TODO: Request for more posts
     const INSTAGRAM_DOMEN = "https://www.instagram.com";
     const LOGIN = "zvzvzvzvzvzvzvzvzvzv";
@@ -18,87 +20,85 @@ class Bot
     //I have no idea how does instagram return`s this param
     const COUNT_OF_POSTS = 7;
 
-    private static $client;
     protected $sessionId;
     protected $csrf_token;
     protected $user_id;
     public $counter;
+
+    public function __construct()
+    {
+        self::$client = new Client();
+        echo "Authentificating..." . PHP_EOL;
+        $this->user = new User(self::$client, self::LOGIN, self::PASSWORD);
+
+    }
 
 
     public function run()
     {
         //TODO: need to add provisional headers to be non-detected
         //TODO: add logger
-        //TODO: set user id
-        $this->user_id = 4277241110;
-        $query_hash = "298b92c8d7cad703f7565aa892ede943";
-
-        echo "Authentificating..." . PHP_EOL;
-
-        self::$client = new Client();
-        $user = new User(self::$client, self::LOGIN, self::PASSWORD);
-        die();
 
         echo "Login success." . PHP_EOL;
 
-        echo "Requesting on hashtag..." . PHP_EOL;
+        $this->likeUserPage("zvzvzvzvzvzvzvzvzvzvzv");
 
-        $headers = [
-            "Cookie" => "sessionid=" . $this->sessionId . ";"
-        ];
 
-        $res = $this->client->request("post", self::INSTAGRAM_DOMEN . "/explore/tags/" . self::HASHTAG . "/?__a=1", [
-            'headers' => $headers
-        ]);
-
-        $result = json_decode($res->getBody());
-        $posts = $result->graphql->hashtag->edge_hashtag_to_media->edges;
-        foreach ($posts as $post) {
-            $this->likePost($post);
-        }
-
-        $end_cursor = $result->graphql->hashtag->edge_hashtag_to_media->page_info->end_cursor;
-
-        while (true) {
-            $variables = [
-                "tag_name" => self::HASHTAG,
-                "first" => self::COUNT_OF_POSTS,
-                "after" => $end_cursor
-            ];
-
-            $headers = [
-                "referer" => self::INSTAGRAM_DOMEN . "/explore/tags/" . self::HASHTAG . "/",
-                "cookie" => "csrftoken=" . $this->csrf_token . "; ds_user_id=" . $this->user_id . "; sessionid=" . $this->sessionId . ";",
-                "x-requested-with" => "XMLHttpRequest"
-            ];
-
-            $url = self::INSTAGRAM_DOMEN . "/graphql/query/?query_hash=" . $query_hash . "&variables=" . json_encode($variables);
-
-            echo "Getting data from " . $url . PHP_EOL;
-
-            $res = $this->client->request("get", $url, [
-                'headers' => $headers
-            ]);
-            $result = json_decode($res->getBody());
-
-            //$posts = $result->graphql->hashtag->edge_hashtag_to_top_posts->edges;
-
-            $posts = $result->data->hashtag->edge_hashtag_to_media->edges;
-
-            foreach ($posts as $post) {
-                $this->likePost($post);
-            }
-            echo $this->counter . PHP_EOL;
-
-            $end_cursor = $result->data->hashtag->edge_hashtag_to_media->page_info->end_cursor;
-        }
+//        echo "Requesting on hashtag..." . PHP_EOL;
+//        $query_hash_tags = "298b92c8d7cad703f7565aa892ede943";
+//
+//        $headers = [
+//            "Cookie" => "sessionid=" . $user->getSessionId() . ";"
+//        ];
+//
+//        $res = $this->client->request("post", self::INSTAGRAM_DOMEN . "/explore/tags/" . self::HASHTAG . "/?__a=1", [
+//            'headers' => $headers
+//        ]);
+//
+//        $result = json_decode($res->getBody());
+//        $posts = $result->graphql->hashtag->edge_hashtag_to_media->edges;
+//        foreach ($posts as $post) {
+//            $this->likePost($post);
+//        }
+//
+//        $end_cursor = $result->graphql->hashtag->edge_hashtag_to_media->page_info->end_cursor;
+//
+//        while (true) {
+//            $variables = [
+//                "tag_name" => self::HASHTAG,
+//                "first" => self::COUNT_OF_POSTS,
+//                "after" => $end_cursor
+//            ];
+//
+//            $headers = [
+//                "referer" => self::INSTAGRAM_DOMEN . "/explore/tags/" . self::HASHTAG . "/",
+//                "cookie" => "csrftoken=" . $this->csrf_token . "; ds_user_id=" . $this->user_id . "; sessionid=" . $this->sessionId . ";",
+//                "x-requested-with" => "XMLHttpRequest"
+//            ];
+//
+//            $url = self::INSTAGRAM_DOMEN . "/graphql/query/?query_hash=" . $query_hash . "&variables=" . json_encode($variables);
+//
+//            echo "Getting data from " . $url . PHP_EOL;
+//
+//            $res = $this->client->request("get", $url, [
+//                'headers' => $headers
+//            ]);
+//            $result = json_decode($res->getBody());
+//
+//            //$posts = $result->graphql->hashtag->edge_hashtag_to_top_posts->edges;
+//
+//            $posts = $result->data->hashtag->edge_hashtag_to_media->edges;
+//
+//            foreach ($posts as $post) {
+//                $this->likePost($post);
+//            }
+//            echo $this->counter . PHP_EOL;
+//
+//            $end_cursor = $result->data->hashtag->edge_hashtag_to_media->page_info->end_cursor;
+//        }
     }
 
-
-
-
-
-    private function likePost($post)
+    private function likePost($post_id, $post_code)
     {
         $random = rand(2, 6);
 
@@ -106,27 +106,25 @@ class Bot
 
         sleep($random);
 
-        $post_id = $post->node->id;
-        $post_shortcode = $post->node->shortcode;
         $headers = [
-            "x-csrftoken" => $this->csrf_token,
-            "referer" => "https://www.instagram.com/p/" . $post_shortcode . "/",
-            "cookie" => "csrftoken=" . $this->csrf_token . "; ds_user_id=" . $this->user_id . "; sessionid=" . $this->sessionId . ";",
+            "x-csrftoken" => $this->user->getCsrfToken(),
+            "referer" => "https://www.instagram.com/p/" . $post_code . "/",
+            "cookie" => "csrftoken=" . $this->user->getCsrfToken() . "; ds_user_id=" . $this->user->getUserId() . "; sessionid=" . $this->user->getSessionId() . ";",
             "content-type" => "application/x-www-form-urlencoded",
 
         ];
 
-        $res = $this->client->request("post", self::INSTAGRAM_DOMEN . "/web/likes/" . $post_id . "/like/", [
+        $res = self::$client->request("post", self::INSTAGRAM_DOMEN . "/web/likes/" . $post_id . "/like/", [
             'headers' => $headers
         ]);
 
 
         if ($res->getStatusCode() != 200) {
-            echo "Something wrong with liking post: https://www.instagram.com/p/ " . $post_shortcode . "/" .
-                "   ID : " . $post_id . "   CSRF-Token : " . $this->csrf_token . PHP_EOL;
+            echo "Something wrong with liking post: https://www.instagram.com/p/ " . $post_code . "/" .
+                "   ID : " . $post_id . PHP_EOL;
         } else {
-            echo "LIKED: https://www.instagram.com/p/" . $post_shortcode . "/" .
-                "   ID : " . $post_id . "   CSRF-Token : " . $this->csrf_token . PHP_EOL;
+            echo "LIKED: https://www.instagram.com/p/" . $post_code . "/" .
+                "   ID : " . $post_id . PHP_EOL;
             $this->counter++;
         }
     }
@@ -135,6 +133,30 @@ class Bot
     {
         $response = self::$client->request($method, $url, $params);
         return $response;
+    }
+
+    private function likeUserPage($login)
+    {
+        $headers = [
+            "Cookie" => "sessionid=" . $this->user->getSessionId() . ";"
+        ];
+
+        $res = self::$client->request("get", self::INSTAGRAM_DOMEN . "/" . $login . "/", [
+            'headers' => $headers
+        ]);
+
+        $content = (string)$res->getBody();
+        $pattern = "/media\"\s?:\s?(\{.*\})\s?\,\s?\"saved_media/";
+        preg_match($pattern, $content, $matches, PREG_OFFSET_CAPTURE);
+
+        if (!$matches) {
+            return null;
+        }
+        $r = json_decode($matches[1][0]);
+        foreach ($r->nodes as $node)
+        {
+            $this->likePost($node->id, $node->code);
+        }
     }
 }
 
--- src/User.php
+++ src/User.php
@@ -19,38 +19,40 @@ class User
         $this->authorize($login, $password);
     }
 
-    private function authorize($login, $password): Response
+    private function authorize($login, $password)
     {
         $response = Bot::request('get', Bot::INSTAGRAM_DOMEN);
         $this->setCsrfToken($this->csrfToken($response));
 
-        $headers = [
-            "x-csrftoken" => $this->getCsrfToken(),
-            "referer" => Bot::INSTAGRAM_DOMEN,
-            "cookie" => "csrftoken=" . $this->getCsrfToken() . ";",
-            "origin" => Bot::INSTAGRAM_DOMEN,
-        ];
-
-        $form_params = [
-            'username' => $login,
-            'password' => $password
-        ];
+//        $headers = [
+//            "x-csrftoken" => $this->getCsrfToken(),
+//            "referer" => Bot::INSTAGRAM_DOMEN,
+//            "cookie" => "csrftoken=" . $this->getCsrfToken() . ";",
+//            "origin" => Bot::INSTAGRAM_DOMEN,
+//        ];
+//
+//        $form_params = [
+//            'username' => $login,
+//            'password' => $password
+//        ];
 
         try {
-            $response = Bot::request("post", Bot::INSTAGRAM_DOMEN . "/accounts/login/ajax/", [
-                'headers' => $headers,
-                'form_params' => $form_params
-            ]);
+//            $response = Bot::request("post", Bot::INSTAGRAM_DOMEN . "/accounts/login/ajax/", [
+//                'headers' => $headers,
+//                'form_params' => $form_params
+//            ]);
 
-            $response_body = json_decode($response->getBody());
-
-            if (!$response_body->authenticated) {
-                throw new \Exception("Something was wrong with logining.");
-            }
+//            $response_body = json_decode($response->getBody());
+//
+//            if (!$response_body->authenticated) {
+//                throw new \Exception("Something was wrong with logining.");
+//            }
             $this->isAuthorised = true;
-            $this->setCsrfToken($this->csrfToken($response));
+            //$this->setCsrfToken($this->csrfToken($response));
+            $this->setCsrfToken("qZ34eFyrwn7r6qtAdNWmDOSB0S1E9cKq");
             echo "csrf-token: " . $this->getCsrfToken() . PHP_EOL;
-            $this->setSessionId($this->sessionId($response));
+            //$this->setSessionId($this->sessionId($response));
+            $this->setSessionId("IGSC94cb0fe65054e40fdb70770427ee3149ed5b829a43abbffe31a70151b87cb748%3A13hxBsrOJpQoSwATVepINRoLeSAf1YwF%3A%7B%22_auth_user_id%22%3A4277241110%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%224277241110%3A4ynycKCzZjW9Z3uYZpBvbdAzNar436nq%3A0cac70051f4053a905fde5bd646ad2941d6dee75ad251a6014d9e31c9c025c1d%22%2C%22last_refreshed%22%3A1519903429.5508422852%7D");
             echo "sessionID: " . $this->getSessionId() . PHP_EOL;
             $this->setUserId($this->userId());
             echo "userId: " . $this->getUserId() . PHP_EOL;
@@ -60,8 +62,6 @@ class User
             echo $e->getMessage();
             die();
         }
-
-        return $response;
     }
 
     public function setCsrfToken($value)
@@ -125,31 +125,20 @@ class User
         return $sessionId;
     }
 
-    protected function userId(): string
+    protected function userId(): int
     {
         if (!$this->isAuthorised) return null;
 
-        $headers = [
-            "Cookie" => "sessionid=" . $this->getSessionId() . "; csrftoken=" . $this->getCsrfToken() . ";",
-            "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "accept-encoding" => "gzip, deflate, br",
-            "user-agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-            "accept-language" => "en,uk;q=0.9,ru-RU;q=0.8,ru;q=0.7,en-US;q=0.6",
-            "upgrade-insecure-requests" => "1"
-        ];
+        $content = urldecode($this->getSessionId());
 
-        $response = Bot::request('get', Bot::INSTAGRAM_DOMEN. "/" . $this->login . "/", [
-            'headers' => $headers,
-        ]);
+        $pattern = "/_auth_user_id\":(\d+),/";
 
-        $content = (string) $response->getBody();
-        $pattern = '/fbq\(\'init\', \'(.*?)\'\);/';
         preg_match($pattern, $content, $matches, PREG_OFFSET_CAPTURE);
+
         if (!$matches) {
             return null;
         }
-
-        return $matches[0][1];
+        return $matches[1][0];
     }
 
 }
